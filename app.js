241a6141a6510754950ffa02a7e4cbf793dca9da
gamecharge11 <73420074+gamecharge11@users.noreply.github.com>
2021-04-30 17:18:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const text = document.getElementById('sentence');
+const strt = document.getElementById('start')
+const what = document.getElementById('what')
+const output = document.getElementById('output')
+
+function splitMulti(str, tokens){
+    var tempChar = tokens[0];
+    for(var i = 1; i < tokens.length; i++){
+        str = str.split(tokens[i]).join(tempChar);
+    }
+    str = str.split(tempChar);
+    return str;
+}
+
+function find () {
+    var value = text.value
+    var letters = 0;
+    document.getElementById('output').style.visibility="visible"
+    document.getElementById('show').style.visibility="visible"
+    if (text.value != ""){ 
+        if (what.value == 'word') {
+            var words = value.split(' ')
+            document.getElementById('show').innerHTML="The text you have entered contains " + words.length + " words."
+        } else if (what.value == 'sentence') {
+            var sentences = splitMulti(value, ['?', '.', '!'])
+            document.getElementById('show').innerHTML="The text you have entered contains " + Number(sentences.length - 1) + " sentences."
+        } else if (what.value == 'alphabet') {
+            var split = Array.from(value)
+            for (let i=0;i<split.length;i++) {
+                if (split[i] != " " && split[i] != ".") {
+                    letters++
+                } else {
+                    continue
+                }
+            }
+            document.getElementById('show').innerHTML="The text you have entered contains " + letters + " letters"
+        }
+    } else {
+        alert("Please enter some text")
+    }
+}
+
